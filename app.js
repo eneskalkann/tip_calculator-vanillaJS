8752d0ea0957f244875e6a05b6ca4c10b42e7b90
eneskalkann <eneskalkan36e@gmail.com>
2023-01-22 14:07:55 +0300

    DONE

---
+++ app.js
@@ -0,0 +1,27 @@
+const resetButton = document.getElementById("reset-button")
+const billInput = document.getElementById("billInput")
+const numberOfPeople = document.getElementById("numberOfPeople")
+const tipPercent = document.getElementById("tipPercent")
+const totalCost = document.getElementById("totalCost")
+const perPersonTotal = document.getElementById("perPersonTotal")
+
+const calculate = () => {
+    const bill = Number(billInput.value)
+    const numberOfPeoples  = Number(numberOfPeople.value)
+    const percent = Number(tipPercent.value) / 100
+    const tipCost = bill * percent
+    const total = tipCost + bill
+    const totalPerPeople = total / numberOfPeoples
+    
+    totalCost.innerText = `${total.toFixed(2)}`
+    perPersonTotal.innerText = `${totalPerPeople.toFixed(2)}`
+}
+
+function reset () {
+    billInput.value="";
+    numberOfPeople.value="";
+    tipPercent.value="";
+    totalCost.innerText="0.00";
+    perPersonTotal.innerText="0.00";
+}
+
